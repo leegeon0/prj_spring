a9127fe20d2ac331a1a35c702700a47f6a5f39b8
lgh1891 <lgh1891@naver.com>
2023-06-21 17:28:02 +0900

    index

---
+++ src/main/java/com/program/app/home/HomeController.java
@@ -0,0 +1,38 @@
+package com.program.app.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class HomeController {
+	
+	@RequestMapping(value="/") // value = / : 최상위 도메인
+	public String home() {
+		
+		// 여기에서 서버단에서 필요한 작업을 수행한다.
+		
+		// href = "/resources/css/style.css";
+		
+		// 아래의 jsp 파일 호출
+		return "home";
+	}
+	
+	@RequestMapping(value="../../resources/index.html")
+	public String index() {
+		
+		return "index";
+	}
+	
+	@RequestMapping(value="/home2") // value = / : 최상위 도메인
+	public String home2() {
+		
+		// 여기에서 서버단에서 필요한 작업을 수행한다.
+		
+		// 아래의 jsp 파일 호출
+		return "app/home/home2";
+	}
+	
+	
+	
+}
