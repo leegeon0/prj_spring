1b8d208160b45401779bc64c006a23153246f0fb
lgh1891 <lgh1891@naver.com>
2023-07-27 14:37:35 +0900

    ㅇㅇ

--- src/main/java/com/program/app/infra/code/CodeController.java
+++ src/main/java/com/program/app/infra/code/CodeController.java
@@ -1,18 +1,12 @@
 package com.program.app.infra.code;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.support.RequestContextUtils;
 
 
 
